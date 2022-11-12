d1abdc19ef960d204f2d87e119ce06d872023913
Moody Rahman <moody.byte@gmail.com>
2022-11-12 14:17:40 -0500

    connect to mongodb with env url

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 
 import express from 'express'
-import { authModule } from "./auth.js"
+import * as dotenv from 'dotenv'
+import * as mongoose from "mongoose"
+import { authModule } from "./routes/auth.js"
+
+
+dotenv.config({
+    path: `${process.env.NODE_ENV}.env`
+})
+
 const app = express()
 const port = 3005
 
 app.use("/auth", authModule)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`⚡ backend server at: http://localhost:${port}`)
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log(`🤖 connected to mongodb`)
+    app.listen(port, () => {
+        console.log(`⚡ backend server at: http://localhost:${port}`)
+    })  
 })
