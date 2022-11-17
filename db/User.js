660040e4594fbb0379c438290770e5096f402852
Moody Rahman <moody.byte@gmail.com>
2022-11-17 15:13:55 -0500

    basic user scheme

---
+++ db/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+
+
+const UserSchema = new Schema({
+    username:  String, // String is shorthand for {type: String}
+    hashed_password: String,
+    salt:   String,
+});
+
+const User = mongoose.model('User', UserSchema);
