11d1344bc337f15c2646d92d7e59dd1a18150803
Moody Rahman <moody.byte@gmail.com>
2022-12-03 17:10:59 -0500

    verify token middleware

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import * as jose from 'jose'
+
+
+const verifyJWT = async (req, res, next) => {
+    const { token } = req.body
+
+    const secret = new TextEncoder().encode(
+        `${process.env.JWT_SECRET}`
+    );
+
+    try {
+        const { payload, protectedHeader } = await jose.jwtVerify(token, secret, {
+            issuer: 'backend.reliquary.moodyrahman.com',
+            audience: 'reliquary.moodyrahman.com',
+        })
+        req.username = payload.username
+        next()
+
+    } catch (error) {
+        next({message:"bad token", status:401})
+    }
+}
+
+export { verifyJWT }
